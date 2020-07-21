a9d1eb8a403fb602c364ac11762175ab4c68f9ef
Nathaniel <nathaniel.ollie.smith@gmail.com>
2020-07-21 21:43:22 +0100

    Add project files.

---
+++ GraphDemo/Graph.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphDemo
+{
+    class Graph
+    {
+        private bool[] connections;
+        private int size;
+
+        public Graph(int size)
+        {
+            this.size = size;
+            Console.WriteLine(size * (size - 1) / 2);
+            connections = new bool[size * (size - 1) / 2];
+        }
+
+        public void AddConnection(int A, int B)
+        {
+            connections[Math.Max(A, B) * (Math.Max(A, B) - 1) / 2 + Math.Min(A, B)] = true;
+        }
+
+        public bool GetConnection(int A, int B) => connections[Math.Max(A, B) * (Math.Max(A, B) - 1) / 2 + Math.Min(A, B)];
+
+        public int[] GetAll(int index)
+        {
+            List<int> ret = new List<int>();
+            for (int i = 0; i < size; i++)
+            {
+                if (i != index && GetConnection(index, i))
+                {
+                    ret.Add(i);
+                }
+            }
+            return ret.ToArray();
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(string.Join(", ", connections));
+        }
+    }
+}
---
+++ GraphDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GraphDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph graph = new Graph(5);
+
+            graph.AddConnection(0, 1);
+            graph.AddConnection(0, 2);
+            graph.AddConnection(0, 3);
+            graph.AddConnection(1, 3);
+            graph.AddConnection(1, 4);
+            graph.AddConnection(2, 3);
+            graph.AddConnection(3, 4);
+
+            graph.Print();
+
+            string str;
+            while ((str = Console.ReadLine()).ToLower() != "exit")
+            {
+                string[] strs = str.Split(' ');
+                switch (strs[0].ToLower())
+                {
+                    case "set":
+                        graph.AddConnection(int.Parse(strs[1]), int.Parse(strs[2]));
+                        break;
+                    case "get":
+                        Console.WriteLine(graph.GetConnection(int.Parse(strs[1]), int.Parse(strs[2])));
+                        break;
+                    case "getall":
+                        Console.WriteLine(string.Join(", ", graph.GetAll(int.Parse(strs[1]))));
+                        break;
+                }
+            }
+        }
+    }
+}
